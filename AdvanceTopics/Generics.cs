a0eb6c0f025e336dbf79c1b2ab05a719b9cd1a16
hemantmudgal <hemant.mudgal_cs19@gla.ac.in>
2023-03-20 23:28:36 +0530

    How to achieven generic behaviour

--- AdvanceTopics/Generics.cs
+++ AdvanceTopics/Generics.cs
@@ -43,8 +43,18 @@ public class Book
 }
 
 /*
- * So for every new type we have to create
- * new class of that type which is not productive
- * way for doing things that's why generics
- * came into picture.
+ * So for every new type we have to create new class of that type which is not productive
+ * way for doing things that's why generics came into picture.
+ *
+ * For solving this problem we can Replace any type with object type because object
+ * is parent of any other class in C#.
+ *
+ * But this can create perfomance penalty becuase if use this class to store value types
+ * it has to boxed in the object type first. So this will cause extra boxing and unboxing
+ * which can create a performance penalty on our project. And for reference type upcasting
+ * and downcasting happens each time so that's why we can't use this technique.
+ *
+ *
+ * But in generic we can create class once and we can use it at multiple time and mutliple
+ * places and it doesn't have perfomance penalty.
  */
