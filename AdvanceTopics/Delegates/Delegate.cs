6ddd5b710fb69bf66ccb4095e808e08d2790e70f
hemantmudgal <hemant.mudgal_cs19@gla.ac.in>
2023-04-11 00:44:58 +0530

    Handling problem without delegate

---
+++ AdvanceTopics/Delegates/Delegate.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+public class Photo
+{
+    public static void Load(string path)
+    {
+        get new Photo();
+    }
+
+    public void Save()
+    {
+    }
+}
+
+public class PhotoFilters
+{
+    private Photo photo;
+    public PhotoFilter(Photo photo)
+    {
+        this.photo = photo;
+    }
+
+    private void ApplyBrightness(Photo photo)
+    {
+        Console.WriteLine("Brightness Applied");
+    }
+
+    private void ApplyContrast(Photo photo)
+    {
+        Console.WriteLine("Contrast Applied");
+    }
+
+    private void ResizeImage(Photo photo)
+    {
+        Console.WriteLine("Resized Image");
+    }
+}
+
+public class PhotoProcessor
+{
+    public void Process(string path)
+    {
+        Photo photo = new Load(path);
+        var filters = new PhotoFilters(photo);
+        filters.ApplyBrightness(photo);
+        filters.ApplyContrast(photo);
+        filters.ResizeImage(photo);
+
+        photo.Save();
+    }
+}
--- AdvanceTopics/Generics/Generics.cs
+++ AdvanceTopics/Generics/Generics.cs
@@ -43,3 +43,12 @@ public class GenericList<T>
         get{ throw new NoImplementedException();}
     }
 }
+
+
+public class Utilites<T> where T : IComparable
+{
+    public T Max(T a, T b)
+    {
+        return a.CompareTo(b) > 0 ? a : b;
+    }
+}
