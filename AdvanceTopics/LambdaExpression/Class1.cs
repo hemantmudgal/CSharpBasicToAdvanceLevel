17b8f86c0549b419b239f3c0f0183507c270b4d9
hemantmudgal <hemant.mudgal_cs19@gla.ac.in>
2023-04-11 01:25:48 +0530

    Solving Problem with lambda expressing and delegate

---
+++ AdvanceTopics/LambdaExpression/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Square(5);
+    }
+
+    static int Square(int value)
+    {
+        return value * value;
+    }
+}
