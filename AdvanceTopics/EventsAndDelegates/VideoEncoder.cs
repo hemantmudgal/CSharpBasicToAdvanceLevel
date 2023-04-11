055d4c81ec687121a449273b8e17a953384a6903
hemantmudgal <hemant.mudgal_cs19@gla.ac.in>
2023-04-12 00:07:56 +0530

    Steps for publisher

--- AdvanceTopics/EventsAndDelegates/VideoEncoder.cs
+++ AdvanceTopics/EventsAndDelegates/VideoEncoder.cs
@@ -2,9 +2,22 @@ using System;
 
 public class VideoEncoder
 {
+    //define delegate first.
+    public delegate void VideoEncoderEventHandler(object source, EventArgs args);
+
+    //define an event.
+    public event VideoEncoderEventHandler VideoEncoded;
     public void Enocde(Video video)
     {
         Console.WrieLine("Video Encoded");
         Thread.Sleep(3000);
+        OnVideoEncoded();
+    }
+
+    //Raise an event.
+    protected virtual void OnVideoEncoded()
+    {
+        if (VideoEncoded != null)
+            VideoEncoded(this, EventsArgs.Empty);
     }
 }
