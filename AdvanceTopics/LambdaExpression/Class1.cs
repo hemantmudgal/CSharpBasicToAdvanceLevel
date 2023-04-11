bbb770d349107e60e448613a6f0a9dd50fa71c93
hemantmudgal <hemant.mudgal_cs19@gla.ac.in>
2023-04-11 19:53:12 +0530

    args => expression

--- AdvanceTopics/LambdaExpression/Class1.cs
+++ AdvanceTopics/LambdaExpression/Class1.cs
@@ -2,6 +2,13 @@
 
 public class Program
 {
+    // args => expression
+
+    public int Square(int number)
+    {
+        return number * number;
+    }
+
     static void Main(string[] args)
     {
         Func<int, int> square = number => number * number;
