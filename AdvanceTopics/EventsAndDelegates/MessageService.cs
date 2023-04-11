6ab5318fd33912305d8f3bba748e9e90c4f444b0
hemantmudgal <hemant.mudgal_cs19@gla.ac.in>
2023-04-12 00:41:23 +0530

    Print Title

--- AdvanceTopics/EventsAndDelegates/MessageService.cs
+++ AdvanceTopics/EventsAndDelegates/MessageService.cs
@@ -4,6 +4,6 @@ public class MessageService
 {
     public void OnVideoEncode(object source, VideoEventArgs e)
     {
-        Console.WriteLine("MessageService: Sending a text");
+        Console.WriteLine("MessageService: Sending a text" + e.Video.Title);
     }
 }
