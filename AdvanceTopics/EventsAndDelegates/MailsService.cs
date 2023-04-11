e288d533100e7c03a17dcdc493b7f11857dc9c06
hemantmudgal <hemant.mudgal_cs19@gla.ac.in>
2023-04-12 00:40:04 +0530

    Update MailService EventArgs

--- AdvanceTopics/EventsAndDelegates/MailsService.cs
+++ AdvanceTopics/EventsAndDelegates/MailsService.cs
@@ -2,7 +2,7 @@ using System;
 
 public class MailService
 {
-    public void OnVideoEncode(object source, EventArgs e)
+    public void OnVideoEncode(object source, VideoEventArgs e)
     {
         Console.WriteLine("MailService: Sending an event");
     }
