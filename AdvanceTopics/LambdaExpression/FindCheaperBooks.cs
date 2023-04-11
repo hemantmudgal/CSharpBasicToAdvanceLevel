36096d97d902dcc7c1d82be5afac0efc32427b7d
hemantmudgal <hemant.mudgal_cs19@gla.ac.in>
2023-04-11 20:29:39 +0530

    Fiding cheaper books without lamdba expression

---
+++ AdvanceTopics/LambdaExpression/FindCheaperBooks.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Class1
+{
+    static void Main(string[] args)
+    {
+        var bookRepository = new BookReposiotry();
+        var cheaperBooks = bookRepository.FindAll(IsChepaerThan10Dollar);
+
+        foreach (var cheaperBook in cheaperBooks)
+        {
+            Console.WriteLine(cheaperBook.Title + " => " + cheaperBook.Price);
+        }
+    }
+
+    public bool IsChaperThan10Dollar(Book book)
+    {
+        return book.Price < 10;
+    }
+}
