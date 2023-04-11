c2bf984c63a6e85ea2417edf146e8188c8016758
hemantmudgal <hemant.mudgal_cs19@gla.ac.in>
2023-04-11 20:13:34 +0530

    lambda expression can access all variables which is define on that scope where expression has to be wirtten.

--- AdvanceTopics/LambdaExpression/Class1.cs
+++ AdvanceTopics/LambdaExpression/Class1.cs
@@ -17,5 +17,13 @@ public class Program
         Func<int, int> square = number => number * number; // This delegate takes input as integer and return integer value.
         int output1 = square(5); // we can call Func Delegate by passing the reuired argument in paranthesis.
         //int output2 = Square(5);
+
+
+        /*
+        lambda expression can access all variables which is define on that scope where expression has to be wirtten.
+        */
+        const int factor = 5;
+        Func<int, int> multiplier = n => n * factor;
+        console.WriteLine(10); //output 50 on consle
     }
 }
