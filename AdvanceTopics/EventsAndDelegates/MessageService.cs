c6af50f337c8cf57a1183c9db9cf9e1210e00ee3
hemantmudgal <hemant.mudgal_cs19@gla.ac.in>
2023-04-12 00:40:25 +0530

    Update message service event args

--- AdvanceTopics/EventsAndDelegates/MessageService.cs
+++ AdvanceTopics/EventsAndDelegates/MessageService.cs
@@ -2,7 +2,7 @@ using System;
 
 public class MessageService
 {
-    public void OnVideoEncode(object source, EventArgs e)
+    public void OnVideoEncode(object source, VideoEventArgs e)
     {
         Console.WriteLine("MessageService: Sending a text");
     }
