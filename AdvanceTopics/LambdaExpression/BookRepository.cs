94b7b2709e089d7dd1436e358812c355944cac62
hemantmudgal <hemant.mudgal_cs19@gla.ac.in>
2023-04-11 20:23:23 +0530

    Added book repository which return list of books

---
+++ AdvanceTopics/LambdaExpression/BookRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class BooksRepository
+{
+    public List<Books> GetBooks()
+    {
+        return new Books()
+        {
+            new Book() { Title = "Title1", Price = 5 },
+            new Book() { Title = "Title2", Price = 7 },
+            new Book() { Title = "Title3", Price = 8 }
+        };
+    }
+}
